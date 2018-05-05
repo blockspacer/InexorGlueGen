e6f9b0ad87bf933b80337b1efb601df6fe254614
a_teammate <madoe3@web.de>
2018-05-05 18:19:07 +0200

    adjust render_files
    missing is only the partials rendering

--- inexor/gluegen/render_files.cpp
+++ inexor/gluegen/render_files.cpp
@@ -1,48 +1,60 @@
-#include <fstream>
+
+#include "inexor/gluegen/render_files.hpp"
+#include "inexor/filesystem/path.hpp"
+
+#include <pugiconfig.hpp>
+#include <pugixml.hpp>
 
 #include <kainjow/mustache.hpp>
 
-#include "inexor/filesystem/path.hpp"
-#include "inexor/gluegen/generate_files.hpp"
-#include "inexor/gluegen/fill_templatedata.hpp"
+#include <fstream>
 
-using namespace inexor::filesystem;
+using namespace pugi;
 
 namespace inexor {
-namespace rpc {
 namespace gluegen {
 
-void render_proto_file(const std::string &out_path, const std::string &template_path, TemplateData &tmpldata)
+/// Render a mustache template into a string.
+const std::string render_template(const std::string &tmplate, const TemplateData &tmpldata)
 {
-    const std::string template_file_contents(filecontents(template_path));
-
-    MustacheTemplate tmpl{template_file_contents.c_str()};
+    MustacheTemplate tmpl{tmplate};
     if(!tmpl.is_valid())
         std::cout << "Template file " << template_path << " malformatted: " << tmpl.error_message() << std::endl;
-
-    std::ofstream sink{out_path, std::ofstream::trunc};
-    tmpl.render(tmpldata, [&sink](const std::string& str) {
-        sink << str;
-    });
-    std::cout << "Rendering Proto file completed." << std::endl;
+    return tmpl.render(tmpldata);
 }
 
-void render_cpp_tree_data(const std::string &out_path, const std::string &template_path, TemplateData &tmpldata)
+/// Create a file containing the given content.
+void save_to_file(const std::string &filepath, const std::string &file_content)
 {
-    const std::string template_file_contents(filecontents(template_path));
-
-    MustacheTemplate tmpl{template_file_contents.c_str()};
-    if(!tmpl.is_valid())
-        std::cout << "Template file " << template_path << " malformatted: " << tmpl.error_message() << std::endl;
-
-    std::ofstream sink{out_path, std::ofstream::trunc};
-    tmpl.render(tmpldata, [&sink](const std::string& str) {
-        sink << str;
-    });
-    std::cout << "Rendering C++ GlueGen file completed." << std::endl;
+    std::ofstream sink{filepath, std::ofstream::trunc};
+    sink << file_content;
+    std::cout << "Rendering C++ GlueGen file completed (" << filepath << ")" << std::endl;
 }
 
-
+void render_files(TemplateData &tmpldata, const std::vector<std::string> &partial_files, const std::vector<std::string> &template_files)
+{
+    /*
+    for(const string &file : partial_files)
+    {
+        // 1. Load xml file
+        // 2. find field with entries and iterate over them
+        for(entry : file_xml.entries){
+            // 3. find out how to add a new field here
+            template_base_data[entry.key] = render_template(entry.value, template_base_data);
+        }
+    } */
+    for(const string &file : template_files)
+    {
+        std::unique_ptr<xml_document>& xml;
+        if(!xml->load_file(file.c_str(), parse_default|parse_trim_pcdata))
+        {
+            std::cout << "XML file representing the AST couldn't be parsed: " << file << std::endl;
+            return;
+        }
+        const string &gen_file_content = render_template(xml.child_name("template"), template_base_data);
+        save_to_file(xml.child_name("filename"), gen_file_content);
+    }
 }
+
 }
 }
--- inexor/gluegen/render_files.hpp
+++ inexor/gluegen/render_files.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
 #include <string>
-
-#include "inexor/gluegen/tree.hpp"
-#include "inexor/gluegen/fill_templatedata.hpp"
+#include <vector>
 
 namespace inexor {
-namespace rpc {
 namespace gluegen {
-
-extern void render_proto_file(const std::string &out_path, const std::string &template_path, TemplateData &tmpldata);
-extern void render_cpp_tree_data(const std::string &out_path, const std::string &template_path, TemplateData &tmpldata);
-}
+    /// We load the xml files which are containing definitions of mustache partials, or the filename plus the mustache
+    /// template for a file we want to generate using the templatedata given.
+    extern void render_files(TemplateData &tmpldata, const std::vector<std::string> &partial_files,
+                             const std::vector<std::string> &template_files);
 }
 }
